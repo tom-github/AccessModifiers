d288b992306cff4a713e8beff8a2accbc5ed84ec
Everton Luiz Pereira <tom.msn@hotmail.com>
2014-07-05 14:17:02 -0300

    Create Repository

---
+++ AccessModifier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+   
+    public class AccessModifier
+    {
+
+        #region Properties
+
+        //The type or member can be accessed by any other code in the same assembly or another assembly that references it.
+        public string PublicModifier { get; set; }
+
+        //The type or member can be accessed only by code in the same class or struct.
+        private string PrivateModifier { get; set; }
+
+        //The type or member can be accessed only by code in the same class or struct, or in a class that is derived from that class.
+        protected string ProtectedModifier { get; set; }
+
+        //The type or member can be accessed by any code in the same assembly, but not from another assembly.
+        //Internal is the default, in the absence of a specified modifier. 
+        internal string IntenalModifier { get; set; }
+        
+        #endregion
+
+        #region Methods
+
+        private void UsingModifiers()
+        {
+            this.PublicModifier = "Public";
+            this.PrivateModifier = "Private";
+            this.ProtectedModifier = "Protected";
+            this.IntenalModifier = "Internal";
+        }
+
+        #endregion
+    }
+}
---
+++ AccessModifierCode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class AccessModifierCode
+    {
+        public void UsingModifiers()
+        {
+            AccessModifier accessModifier = new AccessModifier();
+            accessModifier.PublicModifier = "Public";
+            accessModifier.IntenalModifier = "Internal";
+
+        }
+    }
+
+    class AccessModifierCodeInheritor : AccessModifier
+    {
+        public void UsingModifiers()
+        {
+            base.PublicModifier = "Public";
+            base.ProtectedModifier = "Protected";
+            base.IntenalModifier = "Internal";
+
+        }
+    }
+}
+
